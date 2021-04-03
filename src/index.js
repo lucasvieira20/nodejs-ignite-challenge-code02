83e775359e095d743b4ec1e7c555e27d7e97603c
Lucas Vieira <web.lucasvieira@gmail.com>
2021-04-02 23:26:57 -0300

    feat: resolvendo desafio de middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,72 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find((user) => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: "User already exists" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+ 
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403).json();
+  }
+  
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({
+      error: 'Invalid ID!'
+    });
+  }
+
+  const user = users.find(user => user.username === username)
+  
+
+  if(!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({
+      error: 'Todo do not exist!'
+    });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({
+      error: 'User do not exist!'
+    });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
